cebdf5599d993a288503baf2b2821af8690b81a0
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2023-01-26 16:28:13 +0530

    fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const conroute=require('./Routes/confession')
 const cors=require("cors");
 try{
 
-mongoose.connect("mongodb://localhost:27017/confesslove",{
+mongoose.connect("mongodb+srv://vish:Zekrom9740@cluster0.k5pane6.mongodb.net/?retryWrites=true&w=majority",{
 
     useNewUrlParser:true,
     useUnifiedTopology:true
@@ -28,11 +28,16 @@ app.use(cors());
 app.use(express.json());
 
 
+app.get("/",(req,res)=>{
 
+    res.status(200).json("hello")
+})
 app.use("/api/auth",authroute);
 app.use("/api/confession",conroute);
 
-app.listen(5000,()=>{
+// app.listen( process.env.PORT || 5000,()=>{
 
-    console.log("running..")
-})
+//     console.log("running..")
+// })
+
+module.exports=app
